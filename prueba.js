2668a6fd0f4ae4992e4ada39a7e1a8cda06306eb
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-10-30 16:30:35 -0500

    feat: :tada: Create repository
    Created repository with exercises of Fibonacci and multiplication russian

---
+++ prueba.js
@@ -0,0 +1,86 @@
+/**
+ * TODO: Reto 1: Fibonacci
+ */
+/**
+ ** 1. Escriba un programa que reciba como entrada un número entero n, y entregue como salida el n-
+ **ésimo número de Fibonacci:
+*/
+
+/*let numeroEntrada = 11;
+let numeroFibonacci = 0;
+let numeroAnterior = 1;
+let prevAnterior = 0;
+if(numeroEntrada > numeroAnterior){
+    for (let i = 2; i <= numeroEntrada; i++){ 
+        numeroFibonacci = numeroAnterior + prevAnterior;
+        prevAnterior = numeroAnterior;
+        numeroAnterior = numeroFibonacci;
+       
+    }
+    console.log(numeroFibonacci);
+}else{
+    console.log(numeroEntrada);
+}*/
+
+/**
+ * *2. Escriba un programa que reciba como entrada un número entero e indique si es o no un número
+**  de Fibonacci:
+*/
+
+/*let calculoFibonacci = 0;
+let numeroEntrada = 88;
+let numeroAnterior = 1;
+let prevAnterior = 0;
+while(calculoFibonacci < numeroEntrada){
+    calculoFibonacci = numeroAnterior + prevAnterior;
+    prevAnterior = numeroAnterior;
+    numeroAnterior = calculoFibonacci;
+}
+
+if(calculoFibonacci == numeroEntrada){
+    console.log("Es un número de Fibonacci");
+}else{
+    console.log("No es un número de Fibonacci");
+}*/
+
+/**
+ **3. Escriba un programa que muestres los m primeros números de Fibonacci, donde m es un número
+**ingresado por el usuario: 
+*/
+
+/*let numeroEntrada = 11;
+let numeroFibonacci = 0;
+let numeroAnterior = 0;
+let prevAnterior = 0;
+
+for (let i = 0; i <= numeroEntrada; i++){ 
+    if(i === 1){
+        numeroAnterior++;
+    }
+    numeroFibonacci = numeroAnterior + prevAnterior;
+    if(i > 1){
+        prevAnterior = numeroAnterior;
+    }
+    numeroAnterior = numeroFibonacci;
+    console.log(numeroFibonacci);
+}*/
+
+/**
+ * TODO Reto 2: Multiplicación Rusa
+ * *Desarrolle un programa que reciba como entrada el multiplicador y el multiplicando, y entregue
+  **como resultado el producto de ambos, calculado mediante el método de multiplicación rusa.
+ */
+
+/*let multiplicador = 59;
+let multiplicando = 46;
+let suma = 0;
+
+while(multiplicador >= 1){
+    if(multiplicador % 2 === 0){
+    }else{
+        suma = suma + multiplicando; 
+    }
+    multiplicador = parseInt(multiplicador/2);
+    multiplicando = multiplicando * 2;
+}
+console.log(suma); */
